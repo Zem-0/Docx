14bc2a3d3571cb73905e3caf9033c7a732ce7f92
parinith-19 <parinith@hospicepalliativereview.com>
2025-06-25 14:00:56 +0530

    push 4

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -26,7 +26,6 @@ import {
 import { MoreHorizontal, Eye, Download, Trash2 } from 'lucide-react';
 import { format } from 'date-fns';
 import { useToast } from "@/components/ui/use-toast";
-import { Skeleton } from "@/components/ui/skeleton";
 
 const Library = () => {
     const { user } = useAuth();
@@ -127,37 +126,7 @@ const Library = () => {
     };
 
     if (loading) {
-        return (
-            <Card>
-                <CardHeader>
-                    <CardTitle>My Library</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>Name</TableHead>
-                                <TableHead>Type</TableHead>
-                                <TableHead>Size</TableHead>
-                                <TableHead>Last Modified</TableHead>
-                                <TableHead><span className="sr-only">Actions</span></TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {[...Array(5)].map((_, i) => (
-                                <TableRow key={i}>
-                                    <TableCell><Skeleton className="h-4 w-32" /></TableCell>
-                                    <TableCell><Skeleton className="h-4 w-16" /></TableCell>
-                                    <TableCell><Skeleton className="h-4 w-16" /></TableCell>
-                                    <TableCell><Skeleton className="h-4 w-24" /></TableCell>
-                                    <TableCell><Skeleton className="h-8 w-16" /></TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </CardContent>
-            </Card>
-        );
+        return <div>Loading...</div>;
     }
 
     if (error) {
